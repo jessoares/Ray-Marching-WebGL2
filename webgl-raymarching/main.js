0d6229531f69ec035cc6cccba6752ffa9655812f
jessoares <34700778+jessoares@users.noreply.github.com>
2023-05-09 12:34:31 -0300

    Cleaned a bit of the code

--- webgl-raymarching/main.js
+++ webgl-raymarching/main.js
@@ -1,44 +1,26 @@
 var mesh, timer, shaderProgram;
-let uPosition, uTime, uResolution, uRotation;
-let cameraRotation = {x: 0, y: 0, z: 0};
-let cameraPosition = {x: 0, y: 0, z: -3.5};
-let moveSpeed = 0.01, lookSpeed = 0.25;
-// start() is the main function that gets called first by index.html
+let uPosition, uTime, uResolution;
+
 var start = function() {
-    
-    // Initialize the WebGL 2.0 canvas
     initCanvas();
-
-    // Create timer that will be used for fragment shader
     timer = new Timer();
-
-    // Read in, compile, and create a shader program
     shaderProgram = new Shader('vertShader', 'fragShader');
-    // Activate the shader program
     shaderProgram.UseProgram();
 
-    // Set vertices of the mesh to be the canonical screen space
     var vertices = [-1.0, -1.0,
                      1.0,  1.0,
                     -1.0,  1.0,
                      1.0, -1.0];
-    
-    // Set indices for the vertices above
     var indices = [2, 0, 1,
                    1, 0, 3];
-
-    // Create a mesh based upon the defined vertices and indices
     mesh = new Mesh(vertices, indices, shaderProgram);
 
-    // Render the scene
     drawScene();
 };
 
-// starts the canvas and gl
 var initCanvas = function() {
-	canvas = document.getElementById('game-surface');
-    gl = canvas.getContext('webgl2');   // WebGL 2
-
+	canvas = document.getElementById('canvas');
+    gl = canvas.getContext('webgl2');  
 	gl.enable(gl.DEPTH_TEST);
     gl.enable(gl.BLEND);
     gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA); 
@@ -48,34 +30,22 @@ var initCanvas = function() {
 
 var drawScene = function() {
     normalSceneFrame = window.requestAnimationFrame(drawScene);
-
-    // Adjust scene for any canvas resizing
     resize(gl.canvas);
-    // Update the viewport to the current canvas size
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-    // Set background color to sky blue, used for debug purposes
     gl.clearColor(0.53, 0.81, 0.92, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    // Update the timer
     timer.Update();
-    // Set uniform values of the fragment shader
     shaderProgram.SetUniformVec2("resolution", [gl.canvas.width, gl.canvas.height]);
     shaderProgram.SetUniform1f("time", timer.GetTicksInRadians());
-    
-    // Tell WebGL to draw the scene
     mesh.Draw();
 }
 
-// resizes canvas to fit browser window
 var resize = function(canvas) {
-    // Lookup the size the browser is displaying the canvas.
     var displayWidth  = canvas.clientWidth;
     var displayHeight = canvas.clientHeight;
 
-    // Check if the canvas is not the same size.
     if (canvas.width  !== displayWidth || canvas.height !== displayHeight) {
-        // Make the canvas the same size
         canvas.width  = displayWidth;
         canvas.height = displayHeight;
         aspectRatio = displayWidth / displayHeight;
